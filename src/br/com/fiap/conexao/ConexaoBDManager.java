c8c783301fb47706ff1c08201c8924625844985d
Gustavo Petry <gustavopetry@outlook.com>
2020-10-24 17:59:10 -0300

    conexao com o banco e models

--- src/br/com/fiap/conexao/ConexaoBDManager.java
+++ src/br/com/fiap/conexao/ConexaoBDManager.java
@@ -1,5 +1,21 @@
 package br.com.fiap.conexao;
 
-public class ConexaoBDManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConexaoBDManager {
+	
+    public static Connection obterConexao() {
+    	Connection conexao = null;
+    	
+    	try {
+    		Class.forName("oracle.jdbc.driver.OracleDriver");
+    		conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL","rm85903", "030895");
+    	} catch(Exception e) {
+    		e.printStackTrace();	
+    	}
+    
+    	
+    return conexao; 
+    }
 }
