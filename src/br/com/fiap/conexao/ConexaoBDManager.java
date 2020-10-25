342f794d9c5383e843e0c1c49d08928aedb66e23
Paola Santos de Assis <paolasantos.assis@gmail.com>
2020-10-25 09:52:16 -0300

    conexaoBD

--- src/br/com/fiap/conexao/ConexaoBDManager.java
+++ src/br/com/fiap/conexao/ConexaoBDManager.java
@@ -1,5 +1,22 @@
 package br.com.fiap.conexao;
 
-public class ConexaoBDManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConexaoBDManager {
+	
+	public static Connection obterConexao() {
+		Connection conexao = null;
+		
+		try {
+			Class.forName("Oracle.jdbc.driver.OracleDriver");
+			conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm85650", "111193");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conexao;
+		
+	}
+	
 }
